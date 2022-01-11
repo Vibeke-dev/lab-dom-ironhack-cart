357210d0f11624eb415af8feb3f23b8560a05cd7
Vibeke-dev <vibeke.granhoej.joergensen@lego.com>
2022-01-11 20:28:54 +0100

    iteration1-3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,37 @@
 // ITERATION 1
 
+//const { product, connectToBrowser } = require("puppeteer");
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  
+  const price = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
+  let subTotal = price.innerHTML * quantity.value;
+  
+  let subTotalResult = product.querySelector('.subtotal span')
+  subTotalResult.innerHTML = subTotal; 
+  
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let productList = document.getElementsByClassName('product');
+  let subTotal = 0;
+
+  for (let i = 0; i < productList.length; i++){
+    subTotal += updateSubtotal(productList[i]);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerText = subTotal;
 }
 
 // ITERATION 4
